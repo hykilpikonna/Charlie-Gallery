0051a8a5732eeec13f938b5ec36029af7ded39c4
Hykilpikonna <admin@moecraft.cc>
2019-12-23 11:10:47 -0500

    [+] Create function to determine block width

--- src/responsive.ts
+++ src/responsive.ts
@@ -11,6 +11,22 @@ export default class Responsive
         this.minWidth = minWidth;
         this.margin = margin;
     }
+
+    getBlockWidth(currentWidth: number)
+    {
+        // Loop through all the possible numbers on screen
+        [1, 2, 3, 4, 5, 6].forEach(i =>
+        {
+            // Calculate margin width
+            let margins = vhToPx(this.margin) * (i + 1);
+
+            // Calculate block width
+            let blockWidth = (currentWidth - margins) / i;
+
+            // Return block width
+            if (blockWidth > this.minWidth && blockWidth < this.maxWidth) return blockWidth;
+        });
+    }
 }
 
 /**
