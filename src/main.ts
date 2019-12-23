cf00c445d1831195d302a5e81a33cbd9f1169435
Hykilpikonna <admin@moecraft.cc>
2019-12-22 22:03:02 -0500

    [+] Install elementUI

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+// Use Element UI
+import ElementUI from 'element-ui';
+Vue.use(ElementUI, {locale: 'en-us'});
+
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
