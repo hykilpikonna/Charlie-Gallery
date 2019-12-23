f0ccad4f52e899a3a83210229f957d189ede735f
Hykilpikonna <admin@moecraft.cc>
2019-12-23 11:21:11 -0500

    [F] Reomve maxWidth

--- src/responsive.ts
+++ src/responsive.ts
@@ -1,13 +1,11 @@
 
 export default class Responsive
 {
-    maxWidth: number;
     minWidth: number;
     margin: number;
 
-    constructor(maxWidth: number, minWidth: number, margin: number)
+    constructor(minWidth: number, margin: number)
     {
-        this.maxWidth = maxWidth;
         this.minWidth = minWidth;
         this.margin = margin;
     }
