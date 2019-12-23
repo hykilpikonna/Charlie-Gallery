ed804e6ad20ad9fe9d4f4ced67c469f50f5ee24f
Hykilpikonna <admin@moecraft.cc>
2019-12-23 10:08:06 -0500

    [+] Display artwork image and info

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,13 @@
             <div id="title-sub">{{config.title.sub}}</div>
         </div>
 
-        <el-card class="artwork" :body-style="{padding: '0px'}">
-            <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image" alt="">
+        <el-card class="artwork" :body-style="{padding: '0px'}"
+                 v-for="artwork in artworks">
+            <img :src="artwork.imgThumb" class="image" alt="">
             <div style="padding: 14px;">
-                <span>好吃的汉堡</span>
+                <span>{{artwork.title}}</span>
                 <div class="bottom clearfix">
-                    <time class="time"></time>
+                    <time class="time">{{artwork.rawDate}}</time>
                     <el-button type="text" class="button">操作按钮</el-button>
                 </div>
             </div>
