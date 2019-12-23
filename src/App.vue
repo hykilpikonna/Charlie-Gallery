15690037df1ced973a4d1467eec4bce0c229bc62
Hykilpikonna <admin@moecraft.cc>
2019-12-22 22:05:02 -0500

    [+] Create title div

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-  </div>
+    <div id="app">
+        <div id="title">
+
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
+    import {Component, Vue} from 'vue-property-decorator';
 
-@Component
-export default class App extends Vue
-{
+    @Component
+    export default class App extends Vue
+    {
 
-}
+    }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app
+    {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
+
+    #title
+    {
+
+    }
 </style>
