6578aa208cae7425844b754c4fff54c2f3ea0554
Hykilpikonna <admin@moecraft.cc>
2019-12-23 12:09:13 -0500

    [+] Import swipe

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ Vue.config.productionTip = false;
 import ElementUI from 'element-ui';
 Vue.use(ElementUI, {locale: 'en-us'});
 
+// Use picture-swipe
+const VuePictureSwipe = require('vue-picture-swipe');
+Vue.component('vue-picture-swipe', VuePictureSwipe);
+
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
