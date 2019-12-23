6c49cb29b618c98d2a7cf326d646149002fb9091
Hykilpikonna <admin@moecraft.cc>
2019-12-22 22:26:50 -0500

    [S] Remove margin top

--- src/App.vue
+++ src/App.vue
@@ -31,12 +31,11 @@
 <style lang="scss">
     #app
     {
-      font-family: 'Avenir', Helvetica, Arial, sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      text-align: center;
-      color: #2c3e50;
-      margin-top: 60px;
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
     }
 
     #title
