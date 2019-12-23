8c5614000914c7ac29f6ffe6b50e1708b680292b
Hykilpikonna <admin@moecraft.cc>
2019-12-22 23:07:23 -0500

    [S] Center align text

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@
             let dim = `rgba(0, 0, 0, ${config.title.dim / 100})`;
             dim = `linear-gradient(${dim}, ${dim})`;
 
-            return `height: ${config.title.height}vh;`
+            return `height: ${config.title.height}vh; line-height: ${config.title.height}vh;`
                 + `background-image: ${dim}, url(${require('./assets/' + config.title.background)});`
                 + `color: ${config.title.text_color}`;
         }
