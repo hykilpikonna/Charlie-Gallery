96f7b0b4692ccd3a258e9375393e091f63c2e81a
Hykilpikonna <admin@moecraft.cc>
2019-12-23 09:53:05 -0500

    [+] Create artwork card

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,17 @@
             <div id="title-sub">{{config.title.sub}}</div>
         </div>
 
+        <el-card class="artwork" :body-style="{padding: '0px'}">
+            <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image" alt="">
+            <div style="padding: 14px;">
+                <span>好吃的汉堡</span>
+                <div class="bottom clearfix">
+                    <time class="time"></time>
+                    <el-button type="text" class="button">操作按钮</el-button>
+                </div>
+            </div>
+        </el-card>
+
         <div id="footer" :style="footerStyle">
 
         </div>
