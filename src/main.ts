0c7718d5d96134515e70e0ec2fcc2b0786847f7f
Hykilpikonna <admin@moecraft.cc>
2019-12-24 22:42:40 -0500

    [-] Remove unused library

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,6 @@ Vue.config.productionTip = false;
 import ElementUI from 'element-ui';
 Vue.use(ElementUI, {locale: 'en-us'});
 
-// Use picture-swipe
-const VuePictureSwipe = require('vue-picture-swipe');
-Vue.component('vue-picture-swipe', VuePictureSwipe);
-
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
