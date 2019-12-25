35a98a2021782209760f9ca8e89073b93b658161
Hykilpikonna <admin@moecraft.cc>
2019-12-25 17:45:57 -0500

    [+] Create home content

---
+++ src/content/home.js
@@ -0,0 +1,46 @@
+const homeContent =
+`
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget nulla ligula. 
+Cras a lectus accumsan mauris venenatis placerat. 
+Nulla sapien nibh, convallis non imperdiet a, posuere a est. 
+Quisque orci odio, condimentum nec feugiat et, ultricies eget dui. 
+Aenean imperdiet in quam ut tincidunt. Nulla facilisi. 
+Donec varius lorem augue, consequat vulputate massa auctor vitae. 
+Quisque blandit, libero eu finibus vehicula, nibh erat pulvinar massa, 
+aliquam volutpat libero eros et nibh. In eu felis nisl. Duis interdum, 
+elit id porta molestie, risus magna hendrerit augue, nec varius mauris risus sed mi. 
+Nullam egestas sapien vitae urna sodales cursus non vitae lorem. Aliquam ac enim dui. 
+Maecenas efficitur eleifend mi in accumsan. Nam vel rutrum ante.
+
+Morbi accumsan magna efficitur maximus aliquam. Cras sed orci libero. 
+Suspendisse finibus blandit nisl nec laoreet. Vestibulum vitae luctus augue. 
+Ut non purus magna. Cras in iaculis elit. 
+Fusce quis odio dictum, accumsan diam sed, rhoncus nulla.
+
+Curabitur pretium ultricies erat vel placerat. 
+Pellentesque quis leo urna. Vestibulum nec ultricies leo. 
+Curabitur sit amet est eget elit tempus efficitur a nec ex. 
+Morbi volutpat iaculis nibh, at scelerisque massa. 
+Donec pulvinar est id dui suscipit feugiat. 
+Integer mauris risus, tincidunt eget accumsan ac, finibus in arcu. 
+Pellentesque auctor felis sed ex posuere, sit amet mollis odio sodales. 
+Donec ante sapien, luctus vitae commodo non, feugiat vitae orci. 
+Sed eu justo porttitor dolor tempus vestibulum.
+
+Cras auctor justo at justo placerat lacinia. 
+Donec at mauris porttitor, tempor ligula ut, placerat eros. 
+Mauris ante nulla, ultrices id euismod vitae, rhoncus ut ligula. 
+Nullam ac fermentum nunc. Nullam at eleifend nibh, vel cursus diam. 
+Donec vehicula pulvinar nunc nec facilisis. Pellentesque at tempus nisi. 
+Donec tincidunt odio nec risus tincidunt, non bibendum libero vehicula. 
+Nunc euismod tincidunt magna, vitae ultricies sapien euismod at. 
+Ut sed justo ante.
+
+Nulla dapibus consequat lorem non euismod. Nulla at ex nisi. 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Vestibulum ac lectus felis. 
+Vivamus consectetur diam non diam bibendum imperdiet. 
+Sed ut faucibus arcu. Sed sit amet porta lacus. 
+Nunc efficitur, nibh eget interdum tempus, metus massa ultricies velit, 
+ut tempus ex turpis sit amet turpis. Mauris malesuada vel sem placerat finibus.
+`;
