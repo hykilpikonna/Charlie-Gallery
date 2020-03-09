7cb9312adf7e6a838523eba17a01ac86efa5fef9
Charlie <charlie20151017@gmail.com>
2020-03-09 08:34:18 -0400

    updaten photos

--- src/content/Home.vue
+++ src/content/Home.vue
@@ -1,28 +1,39 @@
 <template>
     <div id="home">
         <p>
-        Art to me is an idea, an expression, and most importantly a lifestyle.
-        Visual arts use their surface appearance to evoke deeper inspirations,
-        which can vary based on who the viewer is. Art takes various forms and
-        is not limited to just painting, photography or music even though they
-        might be the most direct to express artistic feelings.
+        Art to me is an idea, an expression, and most importantly a lifestyle. Visual
+        art uses its surface appearance to evoke deeper inspirations, which can vary
+        based on who the viewer is.
         </p>
         <p>
-        My life circles around Art and without it, my personal hobbies would have
-        never developed:  I like the speed and beautiful moves in fencing, mysterious
-        sleight of hand in modern magic, profound and perfectly presented plot in
-        movies, and intense motions in cardistry. Sometimes, what really catches me
-        is just a feeling, a feeling of art which is usually profound, mysterious,
-        and hard to be explained with human language. This feeling often comes
-        from life but goes beyond reality. It encourages me to use different kinds
-        of medias to present what I see as an artist myself, through which I hope
-        will inspire others.
+        Art takes various forms and is not limited to just painting, photography or music,
+        even though they might be the most direct to express artistic feelings. As an
+        artist myself, my life circles around Art, and without it my personal hobbies would
+        have never developed: I like the speed and beautiful moves in fencing, mysterious
+        sleight of hand in modern magic, profound and inspiring plots in movies, and intense
+        motions in cardistry. Sometimes, what really catches me is just a feeling, a feeling
+        of art which is usually profound, mysterious, and hard to be explained with human
+        language. This feeling often comes from life but goes beyond reality. It encourages
+        me to use different kinds of medias to present what I see as an artist myself, through
+        which I hope will inspire others.
         </p>
         <p>
-        Drawing, painting and photography are the three parts I would love to
-        share with on this website. Also, I’d like to share some of my magic
-        teaching videos. If any questions, you are very welcome to email 
-        me (ytian2021@gmail.com).
+        I have been volunteering for many events to serve communities through my ability as an artist,
+        and have participated in teaching arts on multiple service trips and designing things for school
+        and communities in town. For me, art is not only a pursue of beyond-life state, but also a
+        life style through which I embrace the world.
+        </p>
+        <p>
+        This website is created as another of my attempt to serve communities, through which I will
+        sell high-quality reproductions of my artworks and use the money for donation purpose.
+        </p>
+        <p>
+        If you like my artwork and wish to purchase photos or high-quality drawing reproductions,
+        you are very welcome to contact me by e-mail at ytian2021@gmail.com.
+        </p>
+        <p>
+        Late by not least, Special Thanks to Jack Gui from St. John’s Prep high school, who helped
+        me to create this wonderful website through his skillful coding.
         </p>
     </div>
 </template>
